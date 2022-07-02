367417109b80f6d33d2c3f22c8ef3d38d0381367
Ferid Akşahin <83434568+FeridAksahin@users.noreply.github.com>
2022-07-02 16:29:26 +0300

    final edit

--- Algorithms/3 Question/Solutions.cs
+++ Algorithms/3 Question/Solutions.cs
@@ -168,17 +168,3 @@ class Questions
         
      }
 }
-
-/*
-
-Question - 1: Put the 20 positive numbers entered from the keyboard into 2 separate lists as prime and non-prime. (Write using the ArrayList class.)
-
-Block negative entries.
-Print the elements of each array in ascending order.
-Print the number of elements and the average of both arrays to the screen.
-
-Question - 2: Finding the largest 3 and the smallest 3 of the 20 numbers entered from the keyboard, taking the averages of both groups within themselves and adding these averages and
-Write a program that prints the average totals to the console. (Write using the Array class.)
-
-Question - 3: Write a program that stores the vowels in the sentence entered from the keyboard in an array and sorts the elements of the array.
- */
